6bf98dd8c29e45ac5602ca33e89dc5e031e42c85
yura <yura@com.ua>
2018-04-05 19:03:42 +0300

    create error404.php

---
+++ error404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ERROR 404</title>
+        <style>
+            img{
+                width: 100vw;
+            }
+        </style>
+    </head>
+    <body>
+        <img src="images/error404.jpg" alt="error 404"/>
+    </body>
+</html>
